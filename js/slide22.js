276c593d92e821d6fa53a3ad3aece9b7c4e309b8
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-01 12:06:41 -0400

    slide 22

--- js/slide22.js
+++ js/slide22.js
@@ -1,15 +1,16 @@
 function isNumber() {
     
     var userString = window.prompt("Please enter a Number");
-    if(userString = typeof(Number)) {
+    if(userString = Number) {
             console.log(true);
     } else{
         console.log(false);
     };
-}
-
-
-function convertToNumber() {
-
 
 }
+
+
+//function convertToNumber() {
+
+
+//}
